aa30a2a6597893573ed5072ee0268cd651c76781
Gerardo Renovales <gerardo-renovales@MAC-C02PNYUHG8WN.local>
2015-10-19 17:09:06 -0400

    add email validator

--- models/users.js
+++ models/users.js
@@ -10,7 +10,12 @@ var userSchema = new Schema({
 	password: String,
 	firstName: String,
 	lastName: String,
-	email: String,
+	email: {type: String, validate:{
+		validator: function(email){
+			return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+		},
+		message: '{VALUE} is not a valid email'
+	}},
 	createdDate: {type: Date, default:Date.now},
 	lassAccessDate: Date,
 	active: Boolean,
