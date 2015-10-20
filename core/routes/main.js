18314722017d576ecb6b898e82ca5cf21f37d410
camazorro <camazorro@OBSMBA.local>
2015-10-20 09:53:15 -0400

    scatter way

--- core/routes/main.js
+++ core/routes/main.js
@@ -1,5 +1,12 @@
-module.exports = {
+var self = module.exports = {
 	index: function(request,response){
 		response.render('home',{"logo":"http://cognitiodev.com/images/cognitioLogo.png"});
+	},
+	
+	register: function(app){
+		app.get('/',self.index);
 	}
+}
+self.__module ={
+	provides: ['routes/register']
 }
