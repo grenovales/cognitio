66c8cf322b1698e19bcf8ed97231413781c4075a
Gerardo Renovales <camazorro@gmail.com>
2015-10-20 23:47:41 -0400

    make corrections

--- core/models/users.js
+++ core/models/users.js
@@ -6,18 +6,17 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
 	userID: {type: Schema.Types.ObjectId},
-	userName: {type: String,required: true},
-	password: {type: String,required: true},
-	firstName: {type: String,required: true},
-	lastName: {type: String,required: true},
-	email: {type: String,required: true, validate:{
+	userEmail: {type: String,required: true, validate:{
 		validator: function(email){
 			return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
 		},
 		message: '{VALUE} is not a valid email'
-	}},
+	}},	
+	password: {type: String,required: true},
+	firstName: {type: String,required: true},
+	lastName: {type: String,required: true},
 	createdDate: {type: Date, default:Date.now},
-	lassAccessDate: Date,
-	active: {type: Boolean, required: true, default: true},
+	lastAccessDate: Date,
+	isActive: {type: Boolean, required: true, default: true},
 	courses:[],
 });
