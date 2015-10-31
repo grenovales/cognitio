7c5bac5de90317cf9aadaf05f949c8fd896cea91
Gerardo Renovales <camazorro@gmail.com>
2015-10-30 23:23:49 -0400

    add support for logo

--- core/routes/main.js
+++ core/routes/main.js
@@ -1,6 +1,6 @@
 var self = module.exports = {
 	index: function(request,response){
-		response.render('home',{"logo":"http://cognitiodev.com/images/cognitioLogo.png"});
+		response.render('home',{"logo":"/public/cognitioLogo.png"});
 	},
 	
 	register: function(app){
